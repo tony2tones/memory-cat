6bc0c610843c4386fb8edf73bc4ca217de3f466b
Tony <anthonymarques@outlook.com>
2020-07-18 14:46:02 +0200

    adding flip card and create board functions

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-// card options
-    const cardArray = [
-        {
+    // card options
+    const cardArray = [{
             name: 'cat1',
             img: 'images/cat1'
         },
@@ -32,20 +31,52 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             name: 'cat4',
             img: 'images/cat4'
-        }   
+        }
     ]
 
-    const grid = document.querySelector('.grid')
+    cardsChosen = [];
+    cardChosenId = [];
+    cardsWon = [];
+    const grid = document.querySelector('.grid');
 
     // create your board
 
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img');
-            card.setAttribute('src', 'images/blank.png');
+            card.style.height = "100px";
+            card.style.width = "100px";
+            card.setAttribute('src', 'images/back.png');
             card.setAttribute('data-id', i);
-            card
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card);
 
         }
     }
+
+    function checkForMatch() {
+        let cards = document.querySelectorAll('img');
+        const optionOneId = cardsChosenId[0];
+        const optionsTwoId = cardsChosenId[1];
+        if (cardsChosenId[0] === cardsChosen[1]) {
+            alert('You found a match');
+            cards[optionOneId].setAttribute('src', 'images/white.png');
+            cards[optionsTwoId].setAttribute('src', 'images/white.png');
+            cardsWon.push
+
+        }
+    }
+
+    // flip your card
+    function flipCard() {
+        var cardId = this.getAttribute('data-id');
+        cardsChosen.push(cardArray[cardId].name);
+        cardChosenId.puah(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500);
+        }
+
+    }
+    createBoard();
 });
