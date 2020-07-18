7bf86c8d2fda509018038553c01bf654a1644a2c
Tony <anthonymarques@outlook.com>
2020-07-18 16:35:05 +0200

    Match puzzle working!!

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             name: 'blackCat',
-            img: 'images/blackCat'
+            img: 'images/blackCat.png'
         },
         {
             name: 'whiteCat',
@@ -25,68 +25,69 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/whiteCat.png'
         },
         {
-            name: 'cat4',
-            img: 'images/cat4'
+            name: 'brownCat',
+            img: 'images/brownCat.png'
         },
         {
-            name: 'cat4',
-            img: 'images/cat4'
+            name: 'brownCat',
+            img: 'images/brownCat.png'
         }
     ]
 
-    cardsChosen = [];
-    cardChosenId = [];
-    cardsWon = [];
-    const grid = document.querySelector('.grid');
-    const resultDisplayed = document.querySelector('#result');
-    let cardsChosen = [];
-    let cardsChosenId = [];
-    let cardsWon = [];
+    
+    cardArray.sort(() => 0.5 - Math.random())
+    
+    const grid = document.querySelector('.grid')
+    const resultDisplayed = document.querySelector('#result')
+    var cardsChosen = window.cardsChosen || []
+    var cardsChosenId = window.cardsChosen || []
+    var cardsWon = window.cardsWon || []
+    
     // create your board
-
     function createBoard() {
-        for (let i = 0; i < cardArray.length; i++) {
-            var card = document.createElement('img');
-            card.style.height = "100px";
-            card.style.width = "100px";
-            card.setAttribute('src', 'images/back.png');
-            card.setAttribute('data-id', i);
-            card.addEventListener('click', flipCard);
-            grid.appendChild(card);
-
+        for (var i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.style.height = "100px"
+            card.style.width = "100px"
+            card.setAttribute('src', 'images/back.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
         }
     }
 
     function checkForMatch() {
-        let cards = document.querySelectorAll('img');
-        const optionOneId = cardsChosenId[0];
-        const optionTwoId = cardsChosenId[1];
-        if (cardsChosenId[0] === cardsChosen[1]) {
-            alert('You found a match');
-            cards[optionOneId].setAttribute('src', 'images/whiteCat.png');
-            cards[optionTwoId].setAttribute('src', 'images/whiteCat.png');
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'images/whiteCat.png')
+            cards[optionTwoId].setAttribute('src', 'images/whiteCat.png')
             cardsWon.push(cardsChosen)
         } else {
-            cards[optionOneId].setAttribute('src', 'images/back.png');
-            cards[optionTwoId].setAttribute('src', 'images/back.png');
-            alert('Sorry, try again');
+            cards[optionOneId].setAttribute('src', 'images/back.png')
+            cards[optionTwoId].setAttribute('src', 'images/back.png')
+            alert('Sorry, try again')
         }
-        cardsChosen = [];
-        cardsChosenId = [];
+        cardsChosen = []
+        cardsChosenId = []
         resultDisplayed.textContent = cardsWon.length
         if (cardsWon.length === cardsArray.length/2) {
-            resultDisplayed.textContent = 'Congratulations! You Found all the matches';
+            resultDisplayed.textContent = 'Congratulations! You Found all the matches'
         }
     }
 
     // flip your card
     function flipCard() {
         var cardId = this.getAttribute('data-id');
-        cardsChosen.push(cardArray[cardId].name);
-        cardChosenId.puah(cardId);
-        this.setAttribute('src', cardArray[cardId].img);
+        // console.log('DOES THIS ID SHOW id number of image', cardId);
+        console.log('DOES THIS ID SHOW? show image link ', cardArray[cardId].img);
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
         if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500);
+            setTimeout(checkForMatch, 500)
         }
 
     }
